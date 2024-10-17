5afc02a8ac58823aed5edb474f981cd74e59f4a9
yeon jeyeong <yjy1268@gmail.com>
2024-10-17 09:58:56 +0000

    chap4-sys-ok

--- chap4/prob7/main.c
+++ chap4/prob7/main.c
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 	}
 	if ((fp =fopen(argv[1],"rb"))== NULL){
 		fprintf(stderr, "Error Open File\n");
-		return 2;}
+		return 2;
 }
 printf("------------------------------------------\n");
 printf("%10s %6s %6s\n", "StudentId","Name","Score");
@@ -20,7 +20,7 @@ printf("-------------------------------------------\n");
 while (fread(&rec,sizeof(rec),1,fp)>0)
 	if (rec.id!=0)
 	printf("%10d%6s%6d\n",rec.id,rec.name,rec.score);
-	pritnf("-----------------------------------------\n");
+	printf("-----------------------------------------\n");
 	fclose(fp);
 	return 0;
 	}
